257a6c73efc2867f898c9bbd754a820e9229c786
sriharshamajeti <sriharshamajeti@gmail.com>
2024-03-12 09:23:56 -0500

    graphics2D for text and text cursor

--- MyEditor.java
+++ MyEditor.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
@@ -8,9 +9,12 @@ public class MyEditor extends JFrame {
 
     private static final int WIDTH = 800;
     private static final int LENGTH = 800;
+    private static final int MARGIN = 20;
 
     private int cursorX = 0;
     private int cursorY = 0;
+
+    StringBuilder text = new StringBuilder();
 
     public MyEditor() {
         setTitle("My Test Editor");
@@ -23,8 +27,7 @@ public class MyEditor extends JFrame {
                 super.mouseClicked(e);
                 cursorX = e.getX();
                 cursorY = e.getY();
-                System.out.println(cursorX);
-                System.out.println(cursorY);
+                repaint();
             }
         });
 
@@ -33,9 +36,26 @@ public class MyEditor extends JFrame {
             public void keyPressed(KeyEvent e) {
                 super.keyPressed(e);
                 char keyChar = e.getKeyChar();
-                System.out.println(keyChar);
+                if(Character.isDefined(keyChar)) {
+                    text.insert(text.length(), keyChar);
+                    repaint();
+                }
             }
         });
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2D = (Graphics2D)  g;
+        g2D.setFont(new Font("Serif", Font.PLAIN, 24));
+        int x = MARGIN;
+        int y = MARGIN + g2D.getFontMetrics().getHeight();
+        for (String line : text.toString().split("\n")) {
+            g2D.drawString(line, x, y);
+            y += g2D.getFontMetrics().getHeight();
+        }
+
+        g2D.drawLine(cursorX, cursorY, cursorX, cursorY + g2D.getFontMetrics().getHeight());
     }
 
     public static void main(String[] args) {
