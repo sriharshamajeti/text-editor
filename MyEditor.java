72cc81f6bd72cfc72d077018b604c8292ace37b6
sriharshamajeti <sriharshamajeti@gmail.com>
2024-03-11 22:30:20 -0500

    initial commit

---
+++ MyEditor.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MyEditor extends JFrame {
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My Text Editor");
+        frame.setSize(600, 400); // Set initial window size
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
